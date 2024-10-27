88b38799ac972a730850550446959e36f115a80a
M_Zolfaghari <mzolfagharir@gmail.com>
2024-10-27 11:57:36 +0330

    Loop Exercise

---
+++ src/Loops/Whiles.java
@@ -0,0 +1,34 @@
+package Loops;
+import java.util.Scanner;
+public class Whiles {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your * QTY");
+        int userInput = input.nextInt();
+        int i = 0;
+        while (i <= userInput) {
+            int j = 0;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println(" ");
+            i++;
+        }
+
+        System.out.println(" ");
+
+        int ii = 0;
+        while (ii <= userInput) {
+            int jj = userInput;
+            while (jj >= ii) {
+                System.out.print("*");
+                jj--;
+            }
+            System.out.println(" ");
+            ii++;
+        }
+
+    }
+}
