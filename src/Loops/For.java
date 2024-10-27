4d2e6d34f7c656c5ea5e7f381b7cdadb1adf9958
M_Zolfaghari <mzolfagharir@gmail.com>
2024-10-27 11:41:03 +0330

    Loop Exercise

--- src/Loops/For.java
+++ src/Loops/For.java
@@ -5,14 +5,19 @@ public class For {
         Scanner input = new Scanner(System.in);
         System.out.println("Please Enter Your Star");
         int userInput = input.nextInt();
-        for(int i=1; i<userInput; i++){
-            for(int j=userInput; j>i; j--){
+        for(int i=0;i<=userInput;i++){
+            for(int j=0; j<=i;j++){
                 System.out.print("*");
             }
             System.out.println(" ");
         }
-        for(int ii=1; ii<userInput; ii++){
-            for(int jj=1; jj<ii; jj++){
+
+
+        System.out.println(" ");
+
+
+        for(int i=0;i<=userInput;i++){
+            for(int j=userInput; j>=i;j--){
                 System.out.print("*");
             }
             System.out.println(" ");
