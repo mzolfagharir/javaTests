179c5c2f48644e46e013e5527259e82fa20cc98b
M_Zolfaghari <mzolfagharir@gmail.com>
2024-10-27 13:25:11 +0330

    Loop Exercise

---
+++ src/arrays/Single.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class Single {
+
+    public static void main(String[] args) {
+        int[] myarray= new int[25];
+        for(int i=1; i<=myarray.length-1; i++){
+            myarray[i]=(int)(Math.random()*10);
+            System.out.println(myarray[i]);
+        }
+    }
+}
