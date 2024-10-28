63017e5f1762accb4723df708e06283c0daf1396
M_Zolfaghari <mzolfagharir@gmail.com>
2024-10-28 10:54:45 +0330

    Java Arrays Program.

--- src/arrays/Single.java
+++ src/arrays/Single.java
@@ -1,7 +1,6 @@
 //Simple single array to fill with random number
 package arrays;
 public class Single {
-
     public static void main(String[] args) {
         int[] myarray= new int[25];
         for(int i=1; i<=myarray.length-1; i++){
