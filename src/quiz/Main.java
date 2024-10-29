6ef4dceeb3cdf3ee0fa737009d698c15498f4b14
M_Zolfaghari <mzolfagharir@gmail.com>
2024-10-29 08:50:16 +0330

    Simple Quiz App With Java

--- src/quiz/Main.java
+++ src/quiz/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         QuestionsService q = new QuestionsService();
         q.show();
+        q.showScore();
     }
 }
--- src/quiz/QuestionsService.java
+++ src/quiz/QuestionsService.java
@@ -4,6 +4,7 @@ public class QuestionsService {
     Questions[] q = new Questions[5];
     String[] answers = new String[5];
     Scanner sc = new Scanner(System.in);
+    int i =0;
     public QuestionsService(){
         q[0]=new Questions(1,"Which Year java founded?","1995","2000","1996","2001","1995");
         q[1]=new Questions(2,"Int Size?","4","8","16","32","4");
@@ -12,11 +13,23 @@ public class QuestionsService {
         q[4]=new Questions(5,"Which Year java founded?","1995","2000","1996","2001","1995");
     }
     public void show(){
-        int i =0;
         for(Questions all: q){
             System.out.printf("Question Number %d: %s  %s , %s , %s ,%s \n",all.getId(), all.getQuestion(), all.getOp1(), all.getOp2(), all.getOp3(), all.getOp4());
             answers[i] =sc.nextLine();
             i++;
         }
+        }
+    public void showScore(){
+        int score = 0;
+        for(int i=0; i<q.length;i++){
+            Questions chk = q[i];
+            String correctAnswer = chk.getAns();
+            String userAnswer = answers[i];
+            if(userAnswer.equals(correctAnswer)){
+                score+=20;
+            }
+        }
+        System.out.println("Your score is " +  score  + " Out of 100");
     }
-}
+    }
+
