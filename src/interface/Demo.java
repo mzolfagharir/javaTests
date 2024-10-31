da2312fe0792ef901df74ecf1c2ef7cb1361ce4e
M_Zolfaghari <mzolfagharir@gmail.com>
2024-10-31 12:52:58 +0330

    Simple App for interface usage

---
+++ src/interface/Demo.java
@@ -0,0 +1,27 @@
+//Simple App for interface usage
+interface Computer{
+    void code();
+}
+class Laptop implements Computer{
+    public void code() {
+        System.out.println("Coding with laptop");
+    }
+}
+class AllInOne implements Computer{
+    public void code() {
+        System.out.println("Code with Allinone");
+    }
+}
+class Developer{
+    public void run(Computer sys){
+        System.out.println("Im coding with " + sys.toString());
+    }
+}
+public class Demo {
+    public static void main(String[] args) {
+        Computer l = new Laptop();
+        Computer a = new AllInOne();
+        Developer ali = new Developer();
+        ali.run(l);
+    }
+}
