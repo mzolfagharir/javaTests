895b645c577f28e48f209e1f0c5487de957bfd2f
M_Zolfaghari <mzolfagharir@gmail.com>
2024-11-03 08:44:41 +0330

    Java Projects

--- src/handle/NewHandleThrow.java
+++ src/handle/NewHandleThrow.java
@@ -1,9 +1,11 @@
 package handle;
 public class Handle {
     public static void main(String[] args) {
-        int i = 0;
+        int i = 3;
         try {
-            int result = 10 % 0;
+            int result = 10 / i;
+            if(result==0)
+                throw new ArithmeticException();
         }
         catch (ArithmeticException e) {
             System.out.println("Not OK");
