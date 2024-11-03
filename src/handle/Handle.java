012187989f8bf73890d019625a7aad97ddc8daee
M_Zolfaghari <mzolfagharir@gmail.com>
2024-11-03 08:27:02 +0330

    Exception

--- src/handle/Handle.java
+++ src/handle/Handle.java
@@ -1,11 +1,10 @@
 package handle;
-public class Handle{
+public class Handle {
     public static void main(String[] args) {
-        int i=0;
-        try{
-            int result = 10%0;
-        }
-        catch (ArithmeticException e){
+        int i = 0;
+        try {
+            int result = 10 % 0;
+        } catch (ArithmeticException e) {
             System.out.println("Not Ok");
         }
     }
