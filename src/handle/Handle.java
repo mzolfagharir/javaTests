986bad80a879f76eab9f71edd0e40263045aa5ca
M_Zolfaghari <mzolfagharir@gmail.com>
2024-11-03 08:24:00 +0330

    Exception

---
+++ src/handle/Handle.java
@@ -0,0 +1,12 @@
+package handle;
+public class Handle{
+    public static void main(String[] args) {
+        int i=0;
+        try{
+            int result = 10%0;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Not Ok");
+        }
+    }
+}
